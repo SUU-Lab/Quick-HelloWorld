64e546666754078e86bc328f62ab8e20d0c1b2c2
suudai <34059792+n-suudai@users.noreply.github.com>
2022-04-16 00:11:48 +0900

    追加

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Quick-HelloWorld
+
+# Pre-requisites
+
+- Visual Studio 2022
+- CMake
+
+# Clone
+```cmd
+> git clone https://github.com/SUU-Lab/Quick-HelloWorld.git hoge
+```
+
+# Run
+```cmd
+> .\hoge\build_and_run.bat
+```
---
+++ build_and_run.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set CURRENT=%~dp0
+
+set BUILD_DIR=%CURRENT%\build
+set CONFIGURATION=Debug
+
+echo "===== Generate ====="
+cmake.exe ^
+-G "Visual Studio 17 2022" ^
+-A x64 ^
+-DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
+-B%BUILD_DIR% ^
+-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=lib ^
+-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=bin
+
+echo "===== Build ====="
+cmake.exe --build %BUILD_DIR%
+
+echo "===== Run ====="
+%BUILD_DIR%\bin\%CONFIGURATION%\test_app.exe
